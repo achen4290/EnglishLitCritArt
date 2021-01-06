cc05bc1bc7a05e54e9622902b1e7affbf6cecae1
achen4290 <52362806+achen4290@users.noreply.github.com>
2021-01-05 23:16:29 -0500

    - Finished 3d grid setter

--- grid_handler.py
+++ grid_handler.py
@@ -1,3 +1,6 @@
+from random import shuffle
+
+
 def empty_3Dgrid():
     grid = [[[False for i in range(20)] for j in range(20)] for k in range(20)]
     for i in range(20):
@@ -37,3 +40,77 @@ class master_grids:
         self.XZ = [[False for i in range(20)] for j in range(20)]
         self.YZ = [[False for i in range(20)] for j in range(20)]
         self.XY = [[False for i in range(20)] for j in range(20)]
+
+    def set_XYZ_grid(self):
+        self.XYZ = [[[True for i in range(20)] for j in range(20)] for k in range(20)]
+        for i in range(1, 19):
+            for j in range(1, 19):
+                if not self.XZ[i][j]:
+                    for k in range(20):
+                        self.XYZ[i][k][j] = False
+                if not self.YZ[i][j]:
+                    for k in range(20):
+                        self.XYZ[k][i][j] = False
+                if not self.XY[i][j]:
+                    for k in range(20):
+                        self.XYZ[i][j][k] = False
+
+        XZ_count = [[0 for i in range(20)] for j in range(20)]
+        YZ_count = [[0 for i in range(20)] for j in range(20)]
+        XY_count = [[0 for i in range(20)] for j in range(20)]
+
+        face_points = []
+        body_points = []
+
+        # factoring in edges, but not removing them
+        for i in range(20):
+            XZ_count[0][i] += 2
+            XZ_count[i][0] += 2
+            XZ_count[19][i] += 2
+            XZ_count[i][19] += 2
+            YZ_count[0][i] += 2
+            YZ_count[i][0] += 2
+            YZ_count[19][i] += 2
+            YZ_count[i][19] += 2
+            XY_count[0][i] += 2
+            XY_count[i][0] += 2
+            XY_count[19][i] += 2
+            XY_count[i][19] += 2
+
+        # adding face points
+        for i in range(1, 19):
+            for j in range(1, 19):
+                if self.XYZ[0][i][j]:
+                    YZ_count[i][j] += 2
+                    face_points.append([0, i, j])
+                    face_points.append([19, i, j])
+                if self.XYZ[i][0][j]:
+                    XZ_count[i][j] += 2
+                    face_points.append([i, 0, j])
+                    face_points.append([i, 19, j])
+                if self.XYZ[i][j][0]:
+                    XY_count[i][j] += 2
+                    face_points.append([i, j, 0])
+                    face_points.append([i, j, 19])
+
+        # adding body points
+        for i in range(1, 19):
+            for j in range(1, 19):
+                for k in range(1, 19):
+                    if self.XYZ[i][j][k]:
+                        XZ_count[i][k] += 1
+                        YZ_count[j][k] += 1
+                        XY_count[i][j] += 1
+                        body_points.append([i][j][k])
+
+        shuffle(face_points)
+        shuffle(body_points)
+        points = face_points + body_points
+
+        for p in points:
+            x, y, z = p
+            if XZ_count[x][z] > 1 and YZ_count[y][z] > 1 and XY_count[x][y] > 1:
+                self.XYZ[x][y][z] = False
+                XZ_count[x][z] -= 1
+                YZ_count[y][z] -= 1
+                XY_count[x][y] -= 1
